45c6b5cf6f5f8051f971944ce605c4bd41885b45
Eldaristy <Eldarfriedland@gmail.com>
2022-03-24 16:42:44 +0200

    implemented ppu registers r/w by cpu

--- include/ppu.h
+++ include/ppu.h
@@ -2,20 +2,93 @@
 #define PPU_H
 
 #include <stdint.h>
+#include "ppu_bus.h"
 
 //ppu registers
-#define PPUCTRL 0x2000;
-#define PPUMASK 0x2001;
-#define PPUSTATUS 0x2002;
-#define OAMADDR 0x2003;
-#define OAMDATA 0x2004;
-#define PPUSCROLL 0x2005;
-#define PPUADDR 0x2006;
-#define PPUDATA 0x2007;
-#define OAMDMA 0x4014;
+//#define PPUCTRL 0x2000;
+//#define PPUMASK 0x2001;
+//#define PPUSTATUS 0x2002;
+//#define OAMADDR 0x2003;
+//#define OAMDATA 0x2004;
+//#define PPUSCROLL 0x2005;
+//#define PPUADDR 0x2006;
+//#define PPUDATA 0x2007;
+//#define OAMDMA 0x4014;
 
-uint8_t ppu_regs[8];
+//PPU internal registers - "Loopy" registers
 
-static uint8_t pallete[0x40][3];
+//vram_addr and temp_vram_addr - 15 bits long
+union {
+	struct {
+		uint16_t coarse_x : 5;
+		uint16_t coarse_y : 5;
+		uint16_t nametable : 2;
+		uint16_t fine_y_scroll : 3;
+		// LAST BIT IS UNUSED
+	};
+	uint16_t reg;
+} vram_addr, temp_vram_addr;
 
+uint8_t fine_x_scroll; //3 bits long
+uint8_t addr_latch; //1 bit long
+
+union PPUCTRL{ //Access: write
+	struct {
+		uint8_t nametable_x : 1;
+		uint8_t nametable_y : 1;
+		uint8_t increment_mode : 1;
+		uint8_t spr_patterntable : 1;
+		uint8_t bkg_nametable_y : 1;
+		uint8_t spr_size : 1;
+		uint8_t master_slave : 1;
+		uint8_t gen_nmi : 1;
+	};
+	uint8_t reg;
+} ppuctrl;
+
+union PPUMASK { //Access: write
+	struct {
+		uint8_t greyscale : 1;
+		uint8_t show_bkg_left : 1;
+		uint8_t show_spr_left : 1;
+		uint8_t show_bkg : 1;
+		uint8_t show_spr : 1;
+		uint8_t emph_red : 1;
+		uint8_t emph_green : 1;
+		uint8_t emph_blue : 1;
+	};
+	uint8_t reg;
+} ppumask;
+
+union PPUSTATUS { //Access: read
+	struct {
+		uint8_t UNUSED: 5;
+		uint8_t spr_overflow: 1;
+		uint8_t spr_zero: 1;
+		uint8_t vblank: 1;
+	};
+	uint8_t reg;
+} ppustatus;
+
+union OAMADDR{ //Access: write
+	uint8_t reg;
+} oamaddr;
+
+union OAMDATA { //Access: read, write
+	uint8_t reg;
+} oamdata;
+
+union PPUSCROLL { //Access: write x2
+	uint8_t reg;
+} ppuscroll;
+
+union PPUADDR { //Access: write x2
+	uint8_t reg;
+} ppuaddr;
+
+union PPUDATA { //Access: write x2
+		uint8_t reg;
+} ppudata;
+
+void gen_nmi();
 #endif
--- src/cpu_bus.c
+++ src/cpu_bus.c
@@ -1,5 +1,10 @@
 #include "../include/cpu_bus.h"
 
+void gen_nmi() 
+{
+	assert(!"not implemented");
+}
+
 uint8_t cpu_bus_rd(uint16_t addr)
 {
 	uint8_t read_data = 0;
@@ -7,7 +12,46 @@ uint8_t cpu_bus_rd(uint16_t addr)
 		read_data = cpu_ram[addr & (CPU_RAM_SIZE - 1)];
 	}
 	else if (addr < APU_IO_REGS_START) { //ppu registers
-		read_data = ppu_regs[addr - PPU_REGS_START];
+		switch (addr) {
+		case 0x2000: //PPUCTRL
+			read_data = addr_latch;
+			break;
+
+		case 0x2001: //PPUMASK
+			read_data = addr_latch;
+			break;
+
+		case 0x2002: //PPUSTATUS
+			read_data = ppustatus.reg;
+			ppustatus.vblank = 0;
+			addr_latch = 0;
+			break;
+
+		case 0x2003: //OAMADDR
+			read_data = addr_latch;
+			break;
+
+		case 0x2004: //OAMDATA
+			read_data = oamdata.reg;
+			break;
+
+		case 0x2005: //PPUSCROLL
+			read_data = addr_latch;
+			break;
+
+		case 0x2006: //PPUADDR
+			read_data = addr_latch;
+			break;
+
+		case 0x2007: //PPUDATA
+			read_data = ppudata.reg;
+			vram_addr.reg += ppuctrl.increment_mode ? 32 : 1;
+			break;
+
+		case 0x4014: //OAMDMA
+			assert(!"not implemented");
+		}
+
 	} 
 	else if (addr < DISABLED_APU_IO_START) { //apu and io registers
 		assert(!"not implemented");
@@ -26,7 +70,69 @@ void cpu_bus_wr(uint16_t addr, uint8_t val)
 		cpu_ram[addr & (CPU_RAM_SIZE - 1)] = val;
 	}
 	else if (addr < APU_IO_REGS_START) { //ppu registers
-		ppu_regs[addr - PPU_REGS_START] = val;
+		switch (addr) {
+		case 0x2000: //PPUCTRL
+			ppuctrl.reg = val;
+			if (ppustatus.vblank) {
+				gen_nmi();
+			}
+
+			temp_vram_addr.nametable = ppuctrl.nametable_y << 1 | ppuctrl.nametable_x;
+			break;
+
+		case 0x2001: //PPUMASK
+			ppumask.reg = val;
+			break;
+
+		case 0x2002: //PPUSTATUS
+			break;
+
+		case 0x2003: //OAMADDR
+			oamaddr.reg = val;
+			break;
+
+		case 0x2004: //OAMDATA
+			oamdata.reg = val;
+			break;
+
+		case 0x2005: //PPUSCROLL
+			ppuscroll.reg = val;
+
+			if (addr_latch == 0) {
+				temp_vram_addr.coarse_x = val >> 3; //5 higher bits
+				fine_x_scroll = val & 0x07; //3 lower bits
+				addr_latch = 1;
+			}
+			else { //addr_latch == 1
+				temp_vram_addr.coarse_y = val >> 3;
+				temp_vram_addr.fine_y_scroll = val & 0x07;
+				addr_latch = 0;
+			}
+			break;
+
+		case 0x2006: //PPUADDR
+			ppuaddr.reg = val;
+
+			if (addr_latch == 0) {
+				temp_vram_addr.reg &= val << 8 | 0xFF; 
+				temp_vram_addr.reg &= 0x3FFF; // clear highest 2 bits
+				addr_latch = 1;
+			}
+			else { //addr_latch == 1
+				temp_vram_addr.reg &= val | 0x3F00;
+				vram_addr.reg = temp_vram_addr.reg;
+				addr_latch = 0;
+			}
+			break;
+
+		case 0x2007: //PPUDATA
+			ppu_bus_wr(vram_addr.reg, val);
+			vram_addr.reg += (ppuctrl.increment_mode ? 32 : 1);
+			break;
+
+		case 0x4014: //OAMDMA
+			assert(!"not implemented");
+		}
 	}
 	else if (addr < DISABLED_APU_IO_START) { //apu and io registers
 		assert(!"not implemented");
